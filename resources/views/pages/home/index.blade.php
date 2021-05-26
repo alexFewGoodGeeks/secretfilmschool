cdcf80c7f5a5924676a975eb24d4efc2dd77eac4
Hristijan <kde.chris@gmail.com>
2021-05-26 21:59:01 +0200

    homepage

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -4,42 +4,143 @@
 
     @include('pages.includes.header')
     <!-- Header - set the background image for the header in the line below-->
-    <header class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1600x900')">
-        <div class="text-center my-5">
-            <img class="img-fluid rounded-circle mb-4" src="https://dummyimage.com/150x150/6c757d/dee2e6.jpg" alt="..." />
-            <h1 class="text-white fs-3 fw-bolder">Full Width Pics</h1>
-            <p class="text-white-50 mb-0">Landing Page Template</p>
+    <header class="py-5 bg-image-full">
+        <div class="video-heading">
+            <video autoplay muted loop >
+                <source src="{{ asset('images/video.ogv') }}" type="video/ogg">
+            </video>
         </div>
-    </header>
-    <!-- Content section-->
-    <section class="py-5">
-        <div class="container my-5">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <h2>Full Width Backgrounds</h2>
-                    <p class="lead">A single, lightweight helper class allows you to add engaging, full width background images to sections of your page.</p>
-                    <p class="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the universe.</p>
-                </div>
+        <div class="text-center heading-text-wrapper container">
+            <div class="heading-text title-text col-md-8">
+                <h1>discover the unique methodology of movie success</h1>
+                <h2>Lorem ipsum dolor sit amet, consecetur adsipling sedi emunds imposing</h2>
+            </div>
+            <div class="heading-text cta-button col-md-4">
+
             </div>
         </div>
-    </section>
-    <!-- Image element - set the background image for the header in the line below-->
-    <div class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/4ulffa6qoKA/1200x800')">
-        <!-- Put anything you want here! The spacer below with inline CSS is just for demo purposes!-->
-        <div style="height: 20rem"></div>
-    </div>
-    <!-- Content section-->
-    <section class="py-5">
-        <div class="container my-5">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <h2>Engaging Background Images</h2>
-                    <p class="lead">The background images used in this template are sourced from Unsplash and are open source and free to use.</p>
-                    <p class="mb-0">I can't tell you how many people say they were turned off from science because of a science teacher that completely sucked out all the inspiration and enthusiasm they had for the course.</p>
+    </header>
+
+    <section id="featured-section" class="featured-section-wrapper">
+        <div class="container featured-section">
+            <h2 class="d-flex justify-content-center">Featured on</h2>
+            <div class="featured-section-logos d-flex justify-content-center">
+                <div class="featured-logo logo">
+                    <img src="{{ asset('images/bbc_logo.png') }}" alt="BBC logo" title="BBC logo" />
+                </div>
+                <div class="featured-logo logo">
+                    <img src="{{ asset('images/the_guardian_logo.png') }}" alt="The Guardian logo" title="The Guardian logo" />
+                </div>
+                <div class="featured-logo logo">
+                    <img src="{{ asset('images/rtl_logo.png') }}" alt="RTL logo" title="RTL logo" />
+                </div>
+                <div class="featured-logo logo">
+                    <img src="{{ asset('images/rtl_logo.png') }}" alt="RTL logo" title="RTL logo" />
                 </div>
             </div>
         </div>
     </section>
 
-    @include('pages.includes.footer')
+    <section id="inside-sfs" class="inside-sfs-wrapper">
+            <div class="container">
+                <div class="row">
+                <div class="inside-sfs-text col-md-8">
+                    <h2>inside secret film school</h2>
+                    <p>Lorem ipsum amet dolor</p>
+                </div>
+                <div class="inside-sfs-btn col-md-4">
+                    <div class="inside-sfs-content">
+                        <p>ready to start?</p>
+                        <p>Get full access and start today</p>
+                        <a href="" class="sfs-btn sfs-btn-yellow">start now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+    </section>
+
+
+    <section id="sfs-modules" class="sfs-modules-wrapper">
+        <div class="container">
+        <ul class="nav " id="modules-nav">
+            <li class="nav-item col-md-4">
+                <a class="active nav-link module-one module-nav-item" data-toggle="tab" href="#home">
+                    <span class="module-nav-title">Module One</span>
+                    <span class="module-nav-subtitle">How to build your audience</span>
+                </a>
+            </li>
+            <li class="nav-item col-md-4">
+                <a class="nav-link module-two module-nav-item" data-toggle="tab" href="#menu1">
+                    <span class="module-nav-title">Module Two</span>
+                    <span class="module-nav-subtitle">How to win at film vestivals</span>
+                </a>
+            </li>
+            <li class="nav-item col-md-4">
+                <a class="nav-link module-three module-nav-item" data-toggle="tab" href="#menu2">
+                    <span class="module-nav-title">Module Three</span>
+                    <span class="module-nav-subtitle">How to find distribution</span>
+                </a>
+            </li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div id="home" class="container tab-pane active module-content"><br>
+                <h3>MODULE ONE</h3>
+                <h2>HOW TO BUILD YOUR AUDIENCE</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div id="menu1" class="container tab-pane fade module-content"><br>
+                <h3>MODULE TWO</h3>
+                <h2>HOW TO WIN AT FILM FESTIVALS</h2>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="menu2" class="container tab-pane fade module-content"><br>
+                <h3>MODULE THREE</h3>
+                <h2>HOW TO FIND DISTRIBUTION</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section id="testimonials" class="testimonials-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 testimonial-item d-flex">
+                    <div class="testimonial-avatar">
+                        <img src="{{ asset('images/testimonial_1.png') }}" alt="" title="" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p class="testimonial-author">Jeanine Lex</p>
+                    </div>
+                </div>
+                <div class="col-md-6 testimonial-item d-flex">
+                    <div class="testimonial-avatar">
+                        <img src="{{ asset('images/testimonial_2.png') }}" alt="" title="" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p class="testimonial-author">Jeanine Lex</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="faq" class="faq-wrapper">
+        <div class="container faq-content">
+            <h2>FAQ</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </section>
+
+
+
 @endsection
--- resources/views/pages/includes/header.blade.php
+++ resources/views/pages/includes/header.blade.php
@@ -1,7 +1,13 @@
 <!-- Responsive navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a class="navbar-brand" href="#!">Start Bootstrap</a>
+        <a class="navbar-brand" href="#!">
+            <div class="logo">
+                <img src="{{ asset('images/logo.svg') }}"
+                     alt="SecretFilm School Logo"
+                     title="SecretFilm School logo" />
+            </div>
+        </a>
         <div class="d-flex justify-content-end " id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
@@ -9,3 +15,4 @@
         </div>
     </div>
 </nav>
+
